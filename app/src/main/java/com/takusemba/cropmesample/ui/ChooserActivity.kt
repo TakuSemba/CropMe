e6d778a017c7f4257b5ab4a5d4f444f8b6a5788e
takusemba <takusemba.ele@gmail.com>
2020-09-02 20:19:28 +0900

    remove permission

--- app/src/main/java/com/takusemba/cropmesample/ui/ChooserActivity.kt
+++ app/src/main/java/com/takusemba/cropmesample/ui/ChooserActivity.kt
@@ -8,23 +8,18 @@ import androidx.appcompat.app.AppCompatActivity
 import com.takusemba.cropmesample.R
 import com.takusemba.cropmesample.ui.CropActivity.Companion.EXTRA_SHAPE_TYPE
 
-class ChooserActivity : AppCompatActivity() {
+class ChooserActivity : AppCompatActivity(R.layout.activity_chooser) {
 
-  private val samples: Array<String> = arrayOf(
-      RECTANGLE,
-      CIRCLE,
-      CUSTOM)
+  private val samples: Array<String> = arrayOf(RECTANGLE, CIRCLE, CUSTOM)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_chooser)
     val listView = findViewById<ListView>(R.id.sample_list)
     val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, samples)
     listView.adapter = adapter
     listView.setOnItemClickListener { _, _, position, _ ->
       val intent = Intent(this, CropActivity::class.java)
       val layoutId = when (samples[position]) {
-        // TODO pass type and retrieve layoutId in destination Activity.
         RECTANGLE -> R.layout.activity_crop_rectangle
         CIRCLE -> R.layout.activity_crop_circle
         CUSTOM -> R.layout.activity_crop_custom
